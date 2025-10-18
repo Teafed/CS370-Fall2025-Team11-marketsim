0b5a2a4b45fe00baa4f9baa8476358adbf32a121
Teafed <xjakea45x@yahoo.com>
2025-10-18 14:09:44 -0700

    little class for creating a demo account

--- src/main/java/com/tools/BuildDemoAccount.java
+++ src/main/java/com/tools/BuildDemoAccount.java
@@ -1,14 +1,20 @@
-// src/main/java/com/tools/MockAccountFactory.java
 package com.tools;
 
 import com.accountmanager.Account;
 
-public class BuildMockAccount {
-    private Account account;
+public class BuildDemoAccount {
 
-    public Account createMockAccount() {
-        this.account = new Account("Demo Account");
+// might try to integrate into database here
+//    public static void main(String[] args) {
+//        Account account = new Account("Demo Account");
+//        account.depositFunds(42069.0f);
+//        System.out.printf("[BuildDemoAccount]: Created \"" + account.getName() + "\" with balance $%.2f%n", account.getAvailableBalance());
+//    }
+
+    public static Account buildDemoAccount() {
+        Account account = new Account("Demo Account");
         account.depositFunds(42069.0f);
+        System.out.printf("[BuildDemoAccount]: Created \"" + account.getName() + "\" with balance $%.2f%n", account.getAvailableBalance());
         return account;
     }
 }
