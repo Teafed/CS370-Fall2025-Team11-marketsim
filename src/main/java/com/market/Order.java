7e4b43db022494a8e3046f95d4fd38d48bfd8f4e
quinonesaric <quinonesaric@gmail.com>
2025-11-04 14:13:33 -0800

    Added order class defining variables

---
+++ src/main/java/com/market/Order.java
@@ -0,0 +1,23 @@
+package com.market;
+
+import java.time.LocalDate;
+
+//Record type data
+public class Order {
+
+   // private void stock;
+        //stock name -- private string stock
+    private double price;
+        //stock price -- private double  price
+    private LocalDate execution;
+    //time executed -- the user can select through a drop-down menu when the trade is executed. use candle data or current stock price -- Localdate
+
+    private int sharesbought;
+    //shares bought -- private int shares
+
+    private double ordercost;
+    //total cost -- private double cost
+
+   // private void userselect;
+    //handle user input
+}
